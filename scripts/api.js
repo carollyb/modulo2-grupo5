f8c4ab6035225d963db219a955857642d9d26908
Ivan Geier <vilajr.ivan@gmail.com>
2021-12-18 18:23:35 -0300

    add funcao busca filme

---
+++ scripts/api.js
@@ -0,0 +1,14 @@
+const api_key = 'c30ccb91';
+const url = `http://www.omdbapi.com/?apikey=${api_key}&`;
+
+function buscaFilme(nomeFilme) {
+  $.ajax({
+    type: 'GET',
+    url: `${url}t=${nomeFilme}`,
+    success: function (data) {
+      return data;
+    },
+  });
+}
+
+buscaFilme('nightmare before');
