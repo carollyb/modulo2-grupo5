edc96ac6d91ba7fbb433bc53a2c8473c4f46794f
Priscila Rossetto <priscilassouzap@gmail.com>
2021-12-21 17:03:23 -0300

    implentação de validação de email

--- controller/login.js
+++ controller/login.js
@@ -1,31 +1,14 @@
-const init = () => {
-    const validateEmail = (event) =>{
-    }
-    
-    const inputEmail = document.querySelector('input[type="email"]');
-    const inputPassword = document.querySelector('input[type="password"]');
-    const submitButton = document.querySelector('#forgotPass');
-
-    if(submitButton){
-        submitButton.addEventListener('click', (event) =>{
-            event.preventDefault();
-
-            fetch('https://reqres.in/api/login',{
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    email: inputEmail.value,
-                    password: inputPassword.value,
-                })
-            }).then((response) =>{
-                return response.json();
-            }).then((data) =>{
-                console.log(data)
-            })
-        })
-    }
+function validaEmail(email){
+    let regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi;
+    return regex.test(email);
 }
 
-window.onload = init;
+$('.submit').click(function(e){
+    e.preventDefault()
+    var ehEmail = validaEmail($('#email').val())
+    if(ehEmail == true){
+        window.location.replace('../../index.html')
+    }else{
+        alert('E-mail inválido, digite novamente!')
+    }
+})
