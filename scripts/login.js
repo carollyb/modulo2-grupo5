12d5472d79489b0ff7b06a4f70b96e6ae65baa37
Priscila Rossetto <priscilassouzap@gmail.com>
2021-12-21 15:51:41 -0300

    estruturando páginas login.html e password.html

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,31 @@
 const init = () => {
-    console.log()
+    const validateEmail = (event) =>{
+    }
+    
+    const inputEmail = document.querySelector('input[type="email"]');
+    const inputPassword = document.querySelector('input[type="password"]');
+    const submitButton = document.querySelector('#forgotPass');
 
+    if(submitButton){
+        submitButton.addEventListener('click', (event) =>{
+            event.preventDefault();
+
+            fetch('https://reqres.in/api/login',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: inputEmail.value,
+                    password: inputPassword.value,
+                })
+            }).then((response) =>{
+                return response.json();
+            }).then((data) =>{
+                console.log(data)
+            })
+        })
+    }
 }
 
 window.onload = init;
