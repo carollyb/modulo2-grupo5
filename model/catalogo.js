348791c46e6f82d621472dc356f4714f199b3614
Ivan Geier <vilajr.ivan@gmail.com>
2021-12-22 11:27:38 -0300

    update funcao Catalogo para Classe Catalogo

--- model/catalogo.js
+++ model/catalogo.js
@@ -1,47 +1,54 @@
-const baseFilmes = [
-  'tt0038650',
-  'tt0107688',
-  'tt0099785',
-  'tt0319343',
-  'tt3281548',
-  'tt0170016',
-  'tt0203671',
-  'tt2709692',
-  'tt0064612',
-  'tt0338348',
-  'tt4729430',
-  'tt0033045',
-  'tt2056771',
-  'tt1129435',
-  'tt1748122',
-  'tt8858104',
-  'tt0073195',
-  'tt0092005',
-  'tt7162380',
-  'tt0046250',
-  'tt0059725',
-  'tt0043067',
-  'tt0179389',
-  'tt0112471',
-  'tt1421051',
-  'tt0449059',
-  'tt8326624',
-  'tt0245429',
-  'tt7182294',
-  'tt0051561',
-];
+class Catalogo {
+  #baseFilmes = [
+    'tt0038650',
+    'tt0107688',
+    'tt0099785',
+    'tt0319343',
+    'tt3281548',
+    'tt0170016',
+    'tt0203671',
+    'tt2709692',
+    'tt0064612',
+    'tt0338348',
+    'tt4729430',
+    'tt0033045',
+    'tt2056771',
+    'tt1129435',
+    'tt1748122',
+    'tt8858104',
+    'tt0073195',
+    'tt0092005',
+    'tt7162380',
+    'tt0046250',
+    'tt0059725',
+    'tt0043067',
+    'tt0179389',
+    'tt0112471',
+    'tt1421051',
+    'tt0449059',
+    'tt8326624',
+    'tt0245429',
+    'tt7182294',
+    'tt0051561',
+  ];
 
-const catalogo = [];
+  #catalogo = [];
 
-function criaCatalogo() {
-  while (catalogo.length < 5) {
-    let id = randomID();
-    if (catalogo.indexOf(baseFilmes[id]) == -1) {
-      catalogo.push(baseFilmes[id]);
+  criaCatalogo() {
+    this.#catalogo = [];
+    while (this.#catalogo.length < 12) {
+      let id = this.randomID();
+      if (this.#catalogo.indexOf(this.#baseFilmes[id]) == -1) {
+        this.#catalogo.push(this.#baseFilmes[id]);
+      }
     }
   }
-}
 
-function randomID() {
-  return Math.floor(Math.random() * baseFilmes.length);
+  randomID() {
+    return Math.floor(Math.random() * this.#baseFilmes.length);
+  }
+
+  getCatalogo() {
+    return this.#catalogo;
+  }
 }
