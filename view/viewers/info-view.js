3d91f29f548cbe9b439c0d285c69a8ccca2e7e0e
Ivan Geier <vilajr.ivan@gmail.com>
2021-12-28 13:51:26 -0300

    implementacao buscaFilme no menu dapagina info.html

--- view/viewers/info-view.js
+++ view/viewers/info-view.js
@@ -1,44 +1,6 @@
-const api = new Api()
-
-function carregaFilme (){
-  let filmeId = localStorage.getItem('id');
-  let filmeTitulo = localStorage.getItem('titulo');
-
-  if (filmeId){
-    api.buscaFilmeID(filmeId).then((data) => {
-      if (!data.Error){
-        console.log(data)
-        exibeFilme(data)
-        localStorage.removeItem('id')
-      } else {
-        console.log('este filme não existe')
-        $('.container').hide()
-        $('main').append(`<div>
-        <p>Filme não encontrado!</p>
-      </div>`)
-      }
-    })
-  } else if (filmeTitulo){
-    api.buscaFilme(filmeTitulo).then((data) => {
-      if (!data.Error) {
-        console.log(data)
-        exibeFilme(data)
-        localStorage.removeItem('titulo')
-      } else {
-        console.log('este filme não existe')
-        $('.container').hide()
-        $('main').append(`<div>
-        <p class = "text-white filmeNaoEncontrado">Filme não encontrado!<br> Por favor, pesquise outro título no catálogo</p>
-      </div>`)
-      } 
-    })
-  } else {
-    console.log('não tem filme')
-  }
-}
-
-function exibeFilme(data){
-  $('#poster').attr('src',data.Poster);
+function exibeFilme(data) {
+  comFilme();
+  $('#poster').attr('src', data.Poster);
   $('#title').append(`${data.Title}`);
   $('#year').append(`(${data.Year})`);
   $('#director').append(`${data.Director}`);
@@ -47,6 +9,22 @@ function exibeFilme(data){
   $('#plot').append(`${data.Plot}`);
   $('#genre').append(`${data.Genre}`);
   $('#rated').append(`${data.Rated}`);
-
 }
+
+function semFilme() {
+  $('.displayFilme').addClass('d-none');
+  $('.semFilme').removeClass('d-none');
+}
+
+function comFilme() {
+  $('.displayFilme').removeClass('d-none');
+  $('.semFilme').addClass('d-none');
+}
+
+$('.buscarFilme').click((e) => {
+  e.preventDefault();
+  carregaFilme($('.inputFilme').val());
+  $('.inputFilme').val('');
+});
+
 carregaFilme();
