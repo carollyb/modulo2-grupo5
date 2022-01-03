b8b5a8b340b9d7d4d99f6fc2f20b568471e1d338
sinariac <sinariac@gmail.com>
2022-01-03 16:53:14 -0300

    correção do erro de diretório da página info

--- view/viewers/home-view.js
+++ view/viewers/home-view.js
@@ -25,3 +25,13 @@ preencheFilme().then(() => {
     window.location.assign('./view/pages/info.html');
   });
 });
+
+$('.buscarFilme').click((e) => {
+  e.preventDefault();
+  let titulo = $('.inputFilme').val();
+  if (titulo != '') {
+    localStorage.setItem('titulo', titulo);
+    $('.inputFilme').val('');
+    window.location.assign('./view/pages/info.html');
+  }
+});
